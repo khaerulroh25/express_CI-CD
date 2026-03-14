fc24f9748c9d40c7d44c09369d48cf177d6c6cfb
khaerul25 <khaerulroh@gmail.com>
2026-03-14 12:12:07 +0700

    update

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ mesaage: "hello from Bandung" });
+  res.json({ message: "hello from Bandung" });
 });
 
 module.exports = app;
